56e45232323388e70f206993e2665baf5be0fed2
keerthikalla <67358378+keerthikalla@users.noreply.github.com>
2020-06-29 15:14:09 +0530

    upload

---
+++ project/app.py
@@ -0,0 +1,60 @@
+from __future__ import division, print_function
+import sys
+import os
+import glob
+import numpy as np
+from keras.models import load_model
+from keras.preprocessing import image
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from keras import backend
+from tensorflow.keras import backend
+
+import tensorflow as tf
+global graph
+graph = tf.compat.v1.get_default_graph()
+from skimage.transform import resize
+from flask import Flask , request, render_template, redirect, url_for
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+
+app = Flask(__name__)
+MODEL_PATH = 'models/braintumor.h5'
+model = load_model(MODEL_PATH)
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+@app.route('/predict',methods = ['GET','POST'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['image']
+        print("current path")
+        basepath = os.path.dirname(__file__)
+        print("current path", basepath)
+        filepath = os.path.join(basepath,'uploads',f.filename)
+        print("upload folder is ", filepath)
+        f.save(filepath)
+        
+        img = image.load_img(filepath,target_size = (64,64))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x,axis =0)
+        
+        with graph.as_default():
+            preds = model.predict_classes(x)
+            
+            
+            print("prediction",preds)
+            
+        index = ['Tumor is not present','Tumor is present']
+        
+        text = "Result is : " + index[preds[0]]
+        
+    return text
+if __name__ == '__main__':
+    app.run(debug = True, threaded = False)
+        
+        
+        
+    
+    
+    
