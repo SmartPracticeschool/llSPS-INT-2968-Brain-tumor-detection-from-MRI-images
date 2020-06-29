a333ddabda3c6a7b2acf1aa1edf00a6af5c1f5de
keerthikalla <67358378+keerthikalla@users.noreply.github.com>
2020-06-29 14:40:01 +0530

    upload

--- IDG_class.py
+++
@@ -1,2 +0,0 @@
-train_datagen=ImageDataGenerator(rescale=1./225,shear_range=0.2,zoom_range=0.2,horizontal_flip=True)
-test_datagen=ImageGenerator(rescale=1./225)
--- import_lib.py
+++
@@ -1,4 +0,0 @@
-import tensorflow as tf
-import keras
-
-from keras.preprocessing.image import ImageDataGenerator
